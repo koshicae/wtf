0031ab6ce707c66fa1d8eccafd25d51491f3d0e3
koshicae <poduretsksenia@gmail.com>
2023-04-06 18:37:20 +0300

    IA&C

--- lessons/lesson9/l9.py
+++ lessons/lesson9/l9.py
@@ -39,3 +39,42 @@ x = {
     "age":21,
     "city":"Chisinau"
 }
+
+print(json.dumps({'name': 'John', 'age':30}))
+print(json.dumps(['apple', 'bananas']))
+print(json.dumps(('apple', 'bananas')))
+print(json.dumps('hello'))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
+# a Python object (dict)
+x = {
+    'name': 'Mihai',
+    'age': 21,
+    'city': 'Chisinau'
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
+
+x = {
+    'name': 'John',
+    'age': 30,
+    'married': True,
+    'divorced': False,
+    'children': ('Ann', 'Billy'),
+    'pets': None,
+    'cars': [
+        {'model': 'BMW 230', 'mpg': 27.5},
+        {'model': 'Ford Edge', 'mpg':24.1}
+    ]
+}
+
+print(json.dumps(x))
+print(json.dimps(x, indent=4))
